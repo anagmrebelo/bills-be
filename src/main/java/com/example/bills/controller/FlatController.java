6deb0d5d9eb6ee5d56e8db7d2f122d480b7d3b03
arebelo <anagmrebelo@hotmail.com>
2024-04-13 16:12:52 +0200

    Feature: Add getAll Flats route

--- src/main/java/com/example/bills/controller/FlatController.java
+++ src/main/java/com/example/bills/controller/FlatController.java
@@ -1,11 +1,22 @@
 package com.example.bills.controller;
 
+import com.example.bills.model.Flat;
 import com.example.bills.service.FlatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FlatController {
     @Autowired
     FlatService flatService;
+    @GetMapping("/flats")
+    @ResponseStatus(HttpStatus.OK)
+    List<Flat> getAllFlats() {
+        return flatService.getAllFlats();
+    }
 }
--- src/main/java/com/example/bills/service/FlatService.java
+++ src/main/java/com/example/bills/service/FlatService.java
@@ -1,11 +1,18 @@
 package com.example.bills.service;
 
+import com.example.bills.model.Flat;
 import com.example.bills.repository.FlatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FlatService {
     @Autowired
     FlatRepository flatRepository;
+
+    public List<Flat> getAllFlats() {
+        return flatRepository.findAll();
+    }
 }
