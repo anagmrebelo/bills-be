702c825b30d3df47534f645fb8fd9c876fd0e051
Ana Rebelo <66007323+anagmrebelo@users.noreply.github.com>
2024-04-16 19:46:04 +0200

    Revert "Closes #9"

--- src/main/java/com/example/bills/model/Flatmate.java
+++ src/main/java/com/example/bills/model/Flatmate.java
@@ -1,23 +1,32 @@
 package com.example.bills.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class Flatmate {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @NotEmpty
-    private String name;
-    @NotEmpty
-    @ManyToOne
-    @JoinColumn(name = "flat")
-    private Flat flat;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flatmate flatmate = (Flatmate) o;
+        return id == flatmate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
