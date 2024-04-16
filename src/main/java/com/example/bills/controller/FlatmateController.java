330ff006361a7e1ca62e66cd5a8f6262942b16ca
arebelo <anagmrebelo@hotmail.com>
2024-04-16 19:59:03 +0200

    Feature: add GET all flatmates route

--- src/main/java/com/example/bills/controller/FlatmateController.java
+++ src/main/java/com/example/bills/controller/FlatmateController.java
@@ -1,11 +1,23 @@
 package com.example.bills.controller;
 
+import com.example.bills.model.Flatmate;
 import com.example.bills.service.FlatmateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FlatmateController {
     @Autowired
     FlatmateService flatmateService;
+
+    @GetMapping("/flatmates")
+    @ResponseStatus(HttpStatus.OK)
+    List<Flatmate> getAllFlatmates() {
+        return flatmateService.getAllFlatmates();
+    }
 }
--- src/main/java/com/example/bills/service/FlatmateService.java
+++ src/main/java/com/example/bills/service/FlatmateService.java
@@ -1,11 +1,18 @@
 package com.example.bills.service;
 
+import com.example.bills.model.Flatmate;
 import com.example.bills.repository.FlatmateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FlatmateService {
     @Autowired
     FlatmateRepository flatmateRepository;
+
+    public List<Flatmate> getAllFlatmates() {
+        return flatmateRepository.findAll();
+    }
 }
