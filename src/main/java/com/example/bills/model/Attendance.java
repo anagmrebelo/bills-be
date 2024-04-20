206120e826d88a5eb02f646e7706e51043ff2107
arebelo <anagmrebelo@hotmail.com>
2024-04-20 13:02:20 +0200

    Refactor: review AttendanceModel

--- src/main/java/com/example/bills/model/Attendance.java
+++ src/main/java/com/example/bills/model/Attendance.java
@@ -1,18 +1,41 @@
 package com.example.bills.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+import java.time.Month;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ToString
+@EqualsAndHashCode
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "flatmate", "attendance_month" }) })
 public class Attendance {
-    @EmbeddedId
-    private AttendanceId attendanceId;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private int id;
+
+    @ManyToOne
+    @NonNull
+    @JoinColumn(name = "flatmate")
+    private Flatmate flatmate;
+
+    @NonNull
+    @Column(name = "attendance_month")
+    private Month month;
+
     @NonNull
     private Boolean wasPresent;
+
+    public Attendance(@NonNull Flatmate flatmate, @NonNull Month month, @NonNull Boolean wasPresent) {
+        this.flatmate = flatmate;
+        this.month = month;
+        this.wasPresent = wasPresent;
+    }
 }
