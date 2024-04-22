1152cac37d11d451cb651ff2a1bbd5580cc4c01d
arebelo <anagmrebelo@hotmail.com>
2024-04-22 19:09:07 +0200

    Feature: complete bill controller

--- src/main/java/com/example/bills/model/Bill.java
+++ src/main/java/com/example/bills/model/Bill.java
@@ -1,17 +1,29 @@
 package com.example.bills.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.bills.model.utils.BillType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.Month;
 
 @Getter
 @EqualsAndHashCode
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "bill_type")
+@DiscriminatorValue("other")
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "flat", "month", "bill_type" }) })
 public class Bill {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private int id;
+    @Digits(integer = 6, fraction = 2)
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "flat")
+    private Flat flat;
+    private Month month;
 }
