f8213f1d328aefc3afbf7fa5698387bd3aba24c3
arebelo <anagmrebelo@hotmail.com>
2024-05-02 18:17:36 +0200

    Feature: add balance tests

---
+++ src/test/java/com/example/bills/controller/BalanceControllerTest.java
@@ -0,0 +1,117 @@
+package com.example.bills.controller;
+
+import com.example.bills.model.Debt;
+import com.example.bills.model.Flat;
+import com.example.bills.model.Flatmate;
+import com.example.bills.model.bill.Bill;
+import com.example.bills.repository.DebtRepository;
+import com.example.bills.repository.FlatRepository;
+import com.example.bills.repository.FlatmateRepository;
+import com.example.bills.repository.bill.BillRepository;
+import com.example.bills.service.DebtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class BalanceControllerTest {
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    DebtService debtService;
+    @Autowired
+    DebtRepository debtRepository;
+    @Autowired
+    FlatRepository flatRepository;
+    @Autowired
+    FlatmateRepository flatmateRepository;
+    @Autowired
+    BillRepository billRepository;
+    private Debt debtOne;
+    private Debt debtTwo;
+    private Flatmate flatmateOne;
+    private Flatmate flatmateTwo;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        Flat flat = new Flat("Gran Via");
+        flat = flatRepository.save(flat);
+        flatmateOne = new Flatmate("Rita", flat);
+        flatmateOne = flatmateRepository.save(flatmateOne);
+
+        flatmateTwo = new Flatmate("Rita", flat);
+        flatmateTwo = flatmateRepository.save(flatmateTwo);
+
+        Bill billOne = new Bill(new BigDecimal("80.23"), flat, Month.of(1));
+        billOne = billRepository.save(billOne);
+
+        Bill billTwo = new Bill(new BigDecimal("100.50"), flat, Month.of(2));
+        billTwo = billRepository.save(billTwo);
+
+        debtOne = new Debt(flatmateOne, billOne, new BigDecimal("12.23"));
+        debtOne = debtRepository.save(debtOne);
+
+        debtTwo = new Debt(flatmateOne, billTwo, new BigDecimal("15.00"));
+        debtTwo = debtRepository.save(debtTwo);
+    }
+
+    @AfterEach
+    void tearDown() {
+        debtRepository.deleteAll();
+        debtRepository.flush();
+        billRepository.deleteAll();
+        billRepository.flush();
+        flatmateRepository.deleteAll();
+        flatmateRepository.flush();
+        flatRepository.deleteAll();
+        flatRepository.flush();
+    }
+
+    @Test
+    void getBalance() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/balance/" + flatmateOne.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        BigDecimal balance = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), BigDecimal.class);
+
+        assertEquals(debtOne.getAmount().add(debtTwo.getAmount()), balance);
+    }
+
+    @Test
+    void getEmptyBalance() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/balance/" + flatmateTwo.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        BigDecimal balance = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), BigDecimal.class);
+
+        assertEquals(new BigDecimal("0"), balance);
+    }
+
+    @Test
+    void getBalanceInvalidFlatmate() throws Exception {
+        int invalidFlatmateId = 100;
+        MvcResult mvcResult = mockMvc.perform(get("/balance/" + invalidFlatmateId))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+}
