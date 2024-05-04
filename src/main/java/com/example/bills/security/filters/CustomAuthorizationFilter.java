2792d3b8964925fe13146b8a6daffee6c0a11579
arebelo <anagmrebelo@hotmail.com>
2024-05-04 20:37:16 +0200

    Fix: Review Filters

--- src/main/java/com/example/bills/security/filters/CustomAuthorizationFilter.java
+++ src/main/java/com/example/bills/security/filters/CustomAuthorizationFilter.java
@@ -36,7 +36,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
             if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 try {
                     String token = authorizationHeader.substring("Bearer ".length());
-                    Algorithm algorithm = Algorithm.HMAC256("not so secret".getBytes());
+                    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
                     JWTVerifier verifier = JWT.require(algorithm).build();
                     DecodedJWT decodedJWT = verifier.verify(token);
                     String username = decodedJWT.getSubject();
